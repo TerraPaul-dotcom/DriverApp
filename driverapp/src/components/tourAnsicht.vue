41717a5312499d95fb8cc49f2515d1f034ae57c0
lofma <schunka@posteo.net>
2020-10-15 12:11:50 +0200

    Feat: grundsätzliche Funktion der Tour Ansicht

--- driverapp/src/components/tourAnsicht.vue
+++ driverapp/src/components/tourAnsicht.vue
@@ -1,120 +1,144 @@
 <template>
   <div id="app">
-  <v-app id="inspire">
-    <v-stepper
-      v-model="e6"
-      vertical
+    <v-app id="inspire">
+      <v-stepper v-model="stepCurrent" vertical>
+        <div v-for="step in tour" :key="step.stopsReihenfolge">
+          <v-stepper-step
+            :step="step.stopsReihenfolge + 1"
+            :complete="stepCurrent > step.stopsReihenfolge + 1"
+            >{{
+              `${tour[step.stopsReihenfolge].straße}, ${
+                tour[step.stopsReihenfolge].ort
+              }`
+            }}
+            <small>{{ `${tour[step.stopsReihenfolge].name}` }}</small>
+          </v-stepper-step>
+
+          <v-stepper-content :step="step.stopsReihenfolge + 1"
+            ><v-btn color="primary" @click.prevent="clickStop()">Stop</v-btn>
+            <v-btn-toggle
+              v-model="einstiegJaNein"
+              tile
+              color="deep-purple accent-3"
+              group
+              v-if="stepStatus > 0"
+            >
+              <v-btn small @click.prevent="auswahlEinstieg()"> Einstieg </v-btn>
+              <v-btn small @click.prevent="auswahlKeinEinstieg()"> Kein Einstieg </v-btn>
+            </v-btn-toggle>
+
+            <v-card class="mx-auto" max-width="400">
+              <v-list>
+                <v-list-item-group v-model="selecteOptionsKeinEinstieg" multiple color="indigo" v-if="stepStatus === 3">
+                  <v-list-item v-for="(item, i) in itemsKeinEinstieg" :key="i">
+                    <v-list-item-content>
+                      <v-list-item-title v-text="item.text"></v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card>
+            <v-btn color="primary" @click.prevent="clickWeiterfahrt()">Weiterfahrt </v-btn>
+          </v-stepper-content>
+        </div>
+      </v-stepper>
+
+    <!-- Snackbar -->
+    <v-snackbar
+      v-model="snackbar"
+      timeout="3000"
     >
-      <v-stepper-step
-        :complete="e6 > 1"
-        step="1"
-      >
-        Waldstraße 15, 74382 Waldhausen
-        <small>Nina Meier</small>
-      </v-stepper-step>
-  
-      <v-stepper-content step="1">
-        <v-card
-          color="grey lighten-1"
-          class="mb-12"
-          height="100px"
-        ><br>Feld öffnet sich bei Auswahl, "kein Einstieg" mit Optionen warum Einstieg nicht erfolgreich.</v-card>
-        
+      {{ snackbarText }}
+
+      <template v-slot:action="{ attrs }">
         <v-btn
-          color="primary"
-          @click="e6 = 2"
+          color="pink"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
         >
-          Person eingestiegen
+          Close
         </v-btn>
-        <v-btn text>
-          Kein Einstieg
-        </v-btn>
-        
-      </v-stepper-content>
-  
-      <v-stepper-step
-        :complete="e6 > 2"
-        step="2"
-      >
-        Ziegelstraße, stadt
-      </v-stepper-step>
-  
-      <v-stepper-content step="2">
-        <v-card
-          color="grey lighten-1"
-          class="mb-12"
-          height="200px"
-        ></v-card>
-        <v-btn
-          color="primary"
-          @click="e6 = 3"
-        >
-          Continue
-        </v-btn>
-        <v-btn text>
-          Cancel
-        </v-btn>
-      </v-stepper-content>
-  
-      <v-stepper-step
-        :complete="e6 > 3"
-        step="3"
-      >
-        Oberstraße, ort
-      </v-stepper-step>
-  
-      <v-stepper-content step="3">
-        <v-card
-          color="grey lighten-1"
-          class="mb-12"
-          height="200px"
-        ></v-card>
-        <v-btn
-          color="primary"
-          @click="e6 = 4"
-        >
-          Continue
-        </v-btn>
-        <v-btn text>
-          Cancel
-        </v-btn>
-      </v-stepper-content>
-  
-      <v-stepper-step step="4">
-        Zwischenstraße, ort
-      </v-stepper-step>
-      <v-stepper-content step="4">
-        <v-card
-          color="grey lighten-1"
-          class="mb-12"
-          height="200px"
-        ></v-card>
-        <v-btn
-          color="primary"
-          @click="e6 = 1"
-        >
-          Continue
-        </v-btn>
-        <v-btn text>
-          Cancel
-        </v-btn>
-      </v-stepper-content>
-    </v-stepper>
-  </v-app>
-</div>
+      </template>
+    </v-snackbar>
+    </v-app>
+  </div>
 </template>
 
 <script>
 export default {
-    data () {
+  data() {
     return {
       e6: 1,
-      
+      tour: [
+        //Test Tour. Array of Objects, ), Reihenfolge beginnt mit 0
+        {
+          straße: "Seebahnstraße 44",
+          ort: "Waldburg",
+          name: "Nina Meier",
+          stopsReihenfolge: 0,
+        },
+        {
+          straße: "Ottostraße 34",
+          ort: "Waldburg",
+          name: "Olga Müller",
+          stopsReihenfolge: 1,
+        },
+        {
+          straße: "Friedensstrasse 23",
+          ort: "Hasenburg",
+          name: "Hannes Fritz",
+          stopsReihenfolge: 2,
+        },
+        {
+          straße: "Eckstraße 22",
+          ort: "Mittfelden",
+          name: "Karsten Hagen",
+          stopsReihenfolge: 3,
+        },
+      ],
+      stepCurrent: 1,
+      stepStatus: 0, //0. Noch keine Eingabe, 1. Stop geklickt 2. Auswahl Einstieg Ja, 3. Auswahl Einstieg Nein, 4. Auswahl Grund kein Einstieg, 5. Weiterfahrt geklickt
+      einstiegJaNein: null,
+      itemsKeinEinstieg: [
+        {
+          text: 'Krankheit'
+        },
+        {
+          text: 'Unklar'
+        }
+      ],
+      selecteOptionsKeinEinstieg: [],
+      snackbar: false,
+      snackbarText: null
+
+    };
+  },
+  computed: {},
+  methods: {
+    clickStop (nummerStop) {
+      this.stepStatus = 1
+      this.einstiegJaNein = null
+      return nummerStop
+    },
+    auswahlEinstieg (nummerStop) {
+      this.stepStatus = 2
+      return nummerStop
+    },
+    auswahlKeinEinstieg (nummerStop) {
+      this.stepStatus = 3
+      return nummerStop
+    },
+    clickWeiterfahrt (nummerStop) {
+      this.stepStatus = 0
+      this.stepCurrent += 1
+      this.snackbarText = 'Zwischenhalt gespeichert' 
+      this.snackbar = true
+      return nummerStop
     }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
