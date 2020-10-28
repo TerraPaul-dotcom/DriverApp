6d61bd072bd2a729d8bd6c80ab50e17d20a7d30b
lofma <schunka@posteo.net>
2020-10-28 14:52:00 +0100

    prepare pull request

---
+++ driverapp/src/components/startStopTour.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-btn
+      rounded
+      block
+      dark
+      elevation="2"
+      v-bind:color="type === false ? 'success' : 'error'"
+      @click.prevent="toggleType()"
+    >
+      {{ text }}
+    </v-btn>
+    <v-btn
+      color="primary"
+      @click.prevent="clickStop()"
+      :disabled="stepStatus > 0"
+      block
+      >Stop</v-btn
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      type: true,
+      text: 'Starte Tour'
+    }
+  },
+  methods: {
+    toggleType() {
+      this.type = !this.type
+      this.text = this.type ? 'Starte Tour' : 'Beende Tour'
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ driverapp/src/components/tourAuswahl.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-dialog
+    v-model="this.$store.getters.dialogTourAuswahl"
+    fullscreen
+    hide-overlay
+    transition="dialog-bottom-transition"
+  >
+    <template v-slot:activator="{}">
+      <!-- <v-btn
+          color="primary"
+          dark
+          v-on="on"
+        >
+          Open Dialog
+        </v-btn> -->
+    </template>
+    <v-card>
+      <v-toolbar dark color="primary">
+        <v-btn icon dark @click.prevent="toggleDisplayTourAuswahl()">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-toolbar-title>Tour Auswahl</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <!-- <v-btn
+              dark
+              text
+              @click="dialogUser = false"
+            >
+              Save
+            </v-btn> -->
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-list three-line subheader>
+        <v-subheader>Auswahl Touren</v-subheader>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>Auswahl Tagestour</v-list-item-title>
+            <v-list-item-subtitle>Wähle eine heutige Tour</v-list-item-subtitle>
+            <v-btn @click="tourAuswaehlen(0)">Tour Vormittag</v-btn>
+            <v-btn @click="tourAuswaehlen(1)">Tour Nachmittag</v-btn>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import beispielTouren from '../Beispieldaten/tourenliste_mit_schuelern_beispiel_anonymisiert.json'
+
+export default {
+  data() {
+    return {
+      beispielTouren: beispielTouren.oTourExtListHeute,
+      notifications: false,
+    }
+  },
+  props: {
+    dialogTourAuswahl: Boolean,
+    on: null
+  },
+  methods: {
+    toggleDisplayTourAuswahl() {
+      this.$store.dispatch('updateTourAuswahl', false)
+    },
+    tourAuswaehlen(nr) {
+      let ausgewaehlteTour = this.beispielTouren[nr]
+      ausgewaehlteTour.tourAbschnitte.forEach(element => { //Da die Schülerdaten manchmal in schuelerKurz und manchmal in tourenPunktSchule stehen, kopiere falls null
+        if (element.schuelerKurz === null) {
+          element.schuelerKurz = element.tourenPunktSchule
+        }
+      })
+      this.$store.dispatch('updateTourCurrent',this.beispielTouren[nr])
+      this.$store.dispatch('updateTourAuswahl', false)
+    }
+  },
+  mounted() {
+    this.tourAuswaehlen(0)
+  }
+}
+</script>
+
+<style></style>
