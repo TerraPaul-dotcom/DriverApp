e231019e076406a0e1083fdf2da942e356ca96da
lofma <schunka@posteo.net>
2020-11-17 14:19:50 +0100

    Rework: Console Log entfernen

--- driverapp/src/components/apiKommunikation.vue
+++ driverapp/src/components/apiKommunikation.vue
@@ -8,10 +8,9 @@ import api from '../api.js'
 export default {
     methods: {
         async submitTourenAbgeschlossen(fahrerInput) {
-            console.log(fahrerInput)
         const res = await api.post(`/tourenAbgeschlossen`, fahrerInput)
         if (res.status === 200) {
-            console.log('tourAbgeschlossen übertragen');
+            
         }
       }
     },
